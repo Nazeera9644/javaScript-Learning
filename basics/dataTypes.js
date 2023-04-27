08ce8f6f699f2953244a9122fc4ca955dadac987
Shaik Nazeer Begum <118837825+Nazeera9644@users.noreply.github.com>
2023-04-27 07:38:25 +0000

    data types explained

---
+++ basics/dataTypes.js
@@ -0,0 +1,29 @@
+let name="Nazeera" // string
+let age= 25 // number/Integer
+
+/*The are two types of datatypes in javaScript. They are
+1.Premetive DataTypes
+   Number
+   bigInt
+   String
+   null --> standalone value
+   undefined -->value is not defined
+   symbol ----> used for unique
+
+2.Non-premetive DataTypes
+   Object
+   Arrays
+
+
+   for documentation of datatypes and ecma standards. please refer to "mdn documentation" or "tc39.ec"
+*/
+
+//we can check the data type of the variable using 'typeof' in javaScript.ex
+
+console.log(typeof age) //the output is number
+console.log(typeof name) // string
+
+
+console.log(typeof null) // output is object
+
+console.log(typeof undefined) //output is undefined
