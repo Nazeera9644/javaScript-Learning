b2c31e50bd3b03a72d5ac87ce0ea0c59104f3e27
Shaik Nazeer Begum <118837825+Nazeera9644@users.noreply.github.com>
2023-04-27 10:01:48 +0000

    comparisons explained

---
+++ basics/comparisons.js
@@ -0,0 +1,22 @@
+//Comparisons
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" >= 2);
+// console.log("2" == 2 );
+
+
+// console.log(null >0); false
+// console.log(null ==0);false
+// console.log(null >= 0); true because in javascript comparisons == and > < >= <= works differently
+
+
+//console.log("2" == 2) will be true because == checks only the values
+//console.log("2" === 2) will be false because === checks the value and type of the variable.
+
+
+
